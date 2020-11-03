4ba21ef9b32183f34a463b32b445e02676b341aa
Calterat <Calterat@gmail.com>
2020-11-03 02:22:51 -0600

    functions to populate city are complete including uvIndex check and current weather icon for the city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,72 @@
 let searchFormEl = document.querySelector("#searchForm");
+let uvCheckEl = document.querySelector("#uvCheck");
 let currentDate = moment();
 
-const cityWeatherPopulate = (data) => {
-    console.log(data);
+const fConversion = (K) => {
+    // converts Kelvin to Fahrenheit and rounds to one decimal place
+    let F = Math.round((((K-273.15)*1.8)+32)*10.0)/10.0;
+    return F;
 }
 
-const cityWeatherFetch = (city) => {
+const uviCheck = (uvi) => {
+    // checks UV Index and changes backgorund color according to value
+    if (uvi > 0) {
+        uvCheckEl.classList = '';
+        uvCheckEl.classList = 'bg-success rounded-lg p-1';
+    } 
+    if (uvi > 2) {
+        uvCheckEl.classList = '';
+        uvCheckEl.classList = 'bg-warning rounded-lg p-1';
+    }
+    if (uvi > 5) {
+        uvCheckEl.classList = '';
+        uvCheckEl.classList = 'bg-warning rounded-lg p-1';
+    }
+    if (uvi > 7) {
+        uvCheckEl.classList = '';
+        uvCheckEl.classList = 'btn-danger rounded-lg p-1';
+    }
+}
 
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?APPID=43457cfab621bccace2506c23d4ef384&q=${city}`
+const cityWeatherPopulate = (data, cityName) => {
+
+    console.log(data);
+    // Variables we need from fetch
+    let tempK = data.current.temp; 
+    let tempF = fConversion(tempK)
+    let humidity = data.current.humidity;
+    let wind = data.current.wind_speed;
+    let uvIndex = data.current.uvi;
+    let icon = data.current.weather[0].icon;
+
+    console.log(icon);
+    
+    // Variables from the DOM
+    let citySpan = document.querySelector("#city");
+    let dateSpan = document.querySelector("#date");
+    let tempSpan = document.querySelector("#temp");
+    let humiditySpan = document.querySelector("#humidity");
+    let windSpan = document.querySelector("#windSpeed");
+    let uviSpan = document.querySelector("#uvIndex");
+    let cityWeatherIconImg = document.querySelector("#currentWeatherIcon");
+    cityWeatherIconImg.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+
+    console.log(cityWeatherIconImg);
+
+    // place values in DOM/HTML
+    citySpan.textContent = cityName;
+    dateSpan.textContent = currentDate.format(" (MM/DD/YY) ");
+    tempSpan.textContent = tempF;
+    humiditySpan.textContent = humidity;
+    windSpan.textContent = wind;
+    uviSpan.textContent = uvIndex;
+
+    uviCheck(uvIndex);
+}
+
+const cityOneCallFetch = (cityLat, cityLon, cityName) => {
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=minutely,hourly,alerts&appid=43457cfab621bccace2506c23d4ef384`
     
     // Open Weather Fetching
     fetch(apiUrl)
@@ -19,7 +78,25 @@ const cityWeatherFetch = (city) => {
             }            
         })
         .then(function(response) {
-            cityWeatherPopulate(response);
+            cityWeatherPopulate(response, cityName);
+        })
+}
+
+const cityLatLonFetch = (city) => {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?APPID=43457cfab621bccace2506c23d4ef384&q=${city}`
+    // fetch lat and lon of city from standard call
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                return response.json().then(function(response) {
+                    let cityLat = response.coord.lat;
+                    let cityLon = response.coord.lon;
+                    let cityName = response.name;
+                    cityOneCallFetch(cityLat, cityLon, cityName);
+                });
+            } else {
+                alert(`Error: ${response.statusText}`);
+            }
         })
 }
 
@@ -40,7 +117,7 @@ const collectUserCity = (event) => {
     cityInputEl.value = '';
 
     // send city into api fetch function
-    cityWeatherFetch(cityInput);
+    cityLatLonFetch(cityInput);
     // cityWeatherForcast(cityInput);
 }
 
