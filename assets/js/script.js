9ac399409683716efb78499b891698920893fb50
Calterat <Calterat@gmail.com>
2020-11-03 00:26:02 -0600

    added functions to take user city input and begin api fetches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,46 @@
 let searchFormEl = document.querySelector("#searchForm");
 
+const cityWeatherPopulate = (data) => {
+    console.log(data);
+}
+
+const cityWeatherFetch = (city) => {
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?APPID=43457cfab621bccace2506c23d4ef384&q=${city}`
+    
+    // Open Weather Fetching
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                return response.json();                
+            } else {
+                alert(`Error: ${response.statusText}`);
+            }            
+        })
+        .then(function(response) {
+            cityWeatherPopulate(response);
+        })
+}
+
 const collectUserCity = (event) => {
-    //prevents page from reloading
+    // prevents page from reloading
     event.preventDefault();
 
+    // check if user left text input black
     if (!event.target[0].value) {
         alert("Input a city into the field!");
+        return;
     };
 
+    // grab user input's value
     let cityInputEl = document.querySelector("#citySearch");
     let cityInput = cityInputEl.value
-    // cityInputEl.value = '';
+    // wipe text field after obtaining value
+    cityInputEl.value = '';
 
-    console.log(cityInput);
+    // send city into api fetch function
+    cityWeatherFetch(cityInput);
+    // cityWeatherForcast(cityInput);
 }
 
 // listens for user to submit data
